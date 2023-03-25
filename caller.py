7663a9fd01306b4237c97cadd1354ab33e27cc53
Carlo Bortolan <carlo.bortolan@tum.de>
2023-03-25 14:05:33 +0100

    Implemented default function

--- caller.py
+++ caller.py
@@ -1,12 +1,12 @@
 import argparse
-from typing import List, Dict
+from typing import List, Dict, Any
 
 import yfinance as yf
 
 DEFAULT_STOCKS = ["AAPL", "MSFT", "TSLA"]  # Default stocks to monitor
 
 
-def get_current_stock_prices(stocks: List[str] = None) -> Dict[str, float]:
+def get_current_stock_prices(stocks: List[str] = None) -> Dict[str, Any]:
     """
     Retrieve and display the current stock prices for the given stocks.
 
@@ -31,8 +31,8 @@ def get_current_stock_prices(stocks: List[str] = None) -> Dict[str, float]:
             stock_prices = {}
         for stock, price in zip(stocks, prices):
             stock_prices[stock] = price
-            print(f"{stock}: ${price:.2f}")
-            return stock_prices
+            print(f"{stock}: ${price:}")
+        return stock_prices
     except ValueError as e:
         print(f"Invalid stock symbol: {e}")
         return {}
@@ -44,7 +44,12 @@ def get_current_stock_prices(stocks: List[str] = None) -> Dict[str, float]:
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Retrieve current stock prices.")
     parser.add_argument("-s", "--stocks", nargs="+", help="List of stocks to monitor.")
+    parser.add_argument("-d", "--default_stocks", nargs="+", help="Set the default stocks to monitor.")
     args = parser.parse_args()
+
+    # Set the default stocks if provided
+    if args.default_stocks:
+        DEFAULT_STOCKS = args.default_stocks
 
     # Determine which stocks to monitor
     if args.stocks:
